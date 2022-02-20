52af861821b529827b54ad55daf4d48d7183b67a
Michael Esfahani <esfahanimichael@gmail.com>
2022-02-20 13:05:22 -0800

    updated the way that year information changes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
 import { React, useState, useRef, useEffect } from "react";
 import "./App.css";
-// import { months, days, daysOfWeek } from "./helpers/yearInfo";
+import { months } from "./helpers/yearInfo";
 
 import Month from "./components/month";
 
 function App() {
-  const months = ["January", "February", "March", "April", "May",
-  "June", "July", "August", "September", "October", "November", "December"];
-
-  /// gives currentMonth a number value
   const [currentMonth, changeCurrentMonth] = useState(new Date().getMonth());
   const [currentYearSelected, updateYear] = useState(new Date().getFullYear());
   let monthName = months[currentMonth];
 
-  //gives earlier and later month a str value
   const  [earlierMonth, changeEarlierMonth] = useState(currentMonth-1)
   const [laterMonth, changeLaterMonth] = useState(currentMonth+1)
   const earlierMonthName = months[earlierMonth];
   const laterMonthName = months[laterMonth];
 
-  useEffect() /// useeffect to update the year... once state gets changed for currentMonth, useEffect triggers, lets update the
-  // year based off of current Month. 
-
-  function getMonthInfo() {
-    //  changeCurrentMonth(parseInt(months.indexOf(currentMonth)));
-  }
+  useEffect(() => {
+  });
 
   function getEarlierMonth(){
-    (currentMonth === 0) ? changeCurrentMonth(11) : changeCurrentMonth(currentMonth-1);
+    // (currentMonth === 0) ? (changeCurrentMonth(11), updateYear(currentYearSelected-1)) : changeCurrentMonth(currentMonth-1);
+    if(currentMonth === 0){
+      changeCurrentMonth(11)
+      updateYear(currentYearSelected-1)
+    }
+    else{
+      changeCurrentMonth(currentMonth-1)
+    }
     (earlierMonth === 0) ? changeEarlierMonth(11) : changeEarlierMonth(earlierMonth-1);
     (laterMonth === 0) ? changeLaterMonth(11) : changeLaterMonth(laterMonth-1);
   }
 
   function getLaterMonth(){
-    (currentMonth === 11) ? changeCurrentMonth(0) : changeCurrentMonth(currentMonth+1);
+    // (currentMonth === 11) ? changeCurrentMonth(0) : changeCurrentMonth(currentMonth+1);
+    if(currentMonth === 11){
+      changeCurrentMonth(0)
+      updateYear(currentYearSelected+1)
+    }
+    else{
+      changeCurrentMonth(currentMonth+1)
+    }
     (earlierMonth === 11) ? changeEarlierMonth(0) : changeEarlierMonth(earlierMonth+1);
     (laterMonth === 11) ? changeLaterMonth(0) : changeLaterMonth(laterMonth+1);
   }
- 
-  function getYearInfo() {
-    //updateYear()
-  }
-  // console.log(currentMonth)
-  return (
+   return (
     <div className="App">
       <div>
       <div>{currentYearSelected}</div>
